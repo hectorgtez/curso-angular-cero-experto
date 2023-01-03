7c1753141669e02f4390c4c4bfaf8902a2a0a38a
hectorgtez <hectorgtez@gmail.com>
2023-01-03 12:57:05 -0700

    [ADD] Avance Typescript - Tipos basicos, arreglos, funciones e interfaces

---
+++ 01 Typescript/ts-intro/src/ejercicios/03-funciones.ts
@@ -0,0 +1,36 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+const sumarFlecha = (a: number, b: number): number => {
+    return a + b;
+}
+
+function multiplicar(numero: number, base: number, otroNumero?: number): number {
+    return numero * base;
+}
+
+interface PersonajeLOR {
+    nombre: string;
+    pv: number;
+    mostrarPv: () => void;
+}
+
+function curar(personaje: PersonajeLOR, curarX: number): void {
+    personaje.pv += curarX;
+}
+
+const nuevoPersonaje: PersonajeLOR = {
+    nombre: "Strider",
+    pv: 50,
+    mostrarPv() {
+        console.log("Puntos de vida: ", this.pv);
+    }
+}
+
+curar(nuevoPersonaje, 20)
+nuevoPersonaje.mostrarPv();
---
+++ 01 Typescript/ts-intro/src/ejercicios/04-tarea-tipado.ts
@@ -0,0 +1,32 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Direccion {
+    calle: string;
+    pais: string;
+    ciudad: string;
+}
+
+interface Superheroe {
+    nombre: string;
+    edad: number;
+    direccion: Direccion;
+    mostrarDireccion: () => string;
+}
+
+const superheroe: Superheroe = {
+    nombre: "Spiderman",
+    edad: 30,
+    direccion: {
+        calle: "Main St",
+        pais: "USA",
+        ciudad: "NY"
+    },
+    mostrarDireccion() {
+        return this.nombre + ", " + this.direccion.ciudad + ", " + this.direccion.pais;
+    }
+}
+
+const direccion = superheroe.mostrarDireccion();
+console.log(direccion);
