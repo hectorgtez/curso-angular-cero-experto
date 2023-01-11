661648a8dbd793cde457cfffc24f82288716a93f
hectorgtez <hectorgtez@gmail.com>
2023-01-10 18:40:24 -0700

    [ADD] Avance PaisesApp - Creacion de sidebar y componente PorPais

---
+++ 02 Angular/03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html'
+})
+export class PorPaisComponent {
+  termino: string = "";
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) { }
+
+  buscar() {
+    this.hayError = false;
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe({
+        next: (paises) => {
+          console.log(paises);
+          this.paises = paises;
+        },
+        error: (err) => {
+          this.hayError = true;
+          this.paises = [];
+        }
+      });
+  }
+}
