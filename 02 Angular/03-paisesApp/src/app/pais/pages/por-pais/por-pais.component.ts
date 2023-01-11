072df7fed4b117d6d5df3a14bcfadd6a1a319a5f
hectorgtez <hectorgtez@gmail.com>
2023-01-10 21:22:37 -0700

    [ADD] Seccion PaisesApp Parte 1 terminada

--- 02 Angular/03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 02 Angular/03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -14,13 +14,13 @@ export class PorPaisComponent {
 
   constructor(private paisService: PaisService) { }
 
-  buscar() {
+  buscar(termino: string) {
     this.hayError = false;
+    this.termino = termino;
 
-    this.paisService.buscarPais(this.termino)
+    this.paisService.buscarPais(termino)
       .subscribe({
         next: (paises) => {
-          console.log(paises);
           this.paises = paises;
         },
         error: (err) => {
--- 02 Angular/03-paisesApp/src/app/pais/services/pais.service.ts
+++ 02 Angular/03-paisesApp/src/app/pais/services/pais.service.ts
@@ -7,12 +7,22 @@ import { Country } from '../interfaces/pais.interface';
   providedIn: 'root'
 })
 export class PaisService {
-  private _apiUrl: string = "https://restcountries.com/v3.1";
+  private _apiUrl: string = "https://restcountries.com/v2";
 
   constructor(private http: HttpClient) { }
 
   buscarPais(termino: string): Observable<Country[]> {
     const url = `${this._apiUrl}/name/${termino}`;
     return this.http.get<Country[]>(url);
+  }
+
+  buscarCapital(termino: string): Observable<Country[]> {
+    const url = `${this._apiUrl}/capital/${termino}`;
+    return this.http.get<Country[]>(url);
+  }
+
+  getPaisPorAlpha(id: string): Observable<Country> {
+    const url = `${this._apiUrl}/alpha/${id}`;
+    return this.http.get<Country>(url);
   }
 }
