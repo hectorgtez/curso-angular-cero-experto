2b88979d9edd77f742d342bc1292b35fe09f6922
hectorgtez <hectorgtez@gmail.com>
2023-01-16 17:33:07 -0700

    [ADD] Avance PipesApp - Pipes i18nSelect y i18nPlural

--- 02 Angular/04-pipesApp/src/app/app.component.ts
+++ 02 Angular/04-pipesApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from "primeng/api";
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: string = "Hector";
+  constructor(private primeNgConfig: PrimeNGConfig) { }
 
-  mostrarNombre() {
-
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
   }
 }
--- 02 Angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 02 Angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -5,5 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './no-comunes.component.html'
 })
 export class NoComunesComponent {
+  // i18nSelect
+  nombre: string = "Héctor";
+  genero: string = "masculino";
+  invitacionMapa = {
+    "masculino": "invitarlo",
+    "femenino": "invitarla"
+  }
 
+  // i18nPlural
+  clientes: string[] = ["María", "Pedro", "Juan", "Eduardo", "Hector"];
+  clientesMapa = {
+    "=0": "no tenemos ningún cliente esperando.",
+    "=1": "tenemos un cliente esperando.",
+    "other": "tenemos # clientes esperando.",
+  }
+
+  cambiarNombre(): void {
+    if(this.genero === "masculino") {
+      this.nombre = "Deisy";
+      this.genero = "femenino";
+    } else {
+      this.nombre = "Héctor";
+      this.genero = "masculino";
+    }
+  }
+
+  borrarCliente(): void {
+    if(this.clientes.length === 0) {
+      return;
+    }
+
+    this.clientes.pop();
+  }
 }
