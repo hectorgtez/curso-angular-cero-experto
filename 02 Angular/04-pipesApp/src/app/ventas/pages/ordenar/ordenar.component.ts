ce4bc7601d573b37520640c2b9d4b9429108ced8
hectorgtez <hectorgtez@gmail.com>
2023-01-18 17:27:05 -0700

    [ADD] Seccion PipesApp terminada

--- 02 Angular/04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 02 Angular/04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
 
+import { Color, Heroe } from '../../interfaces/ventas.interface';
+
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html'
 })
 export class OrdenarComponent {
+  enMayusculas: boolean = false;
+  ordenarPor: string = "";
+  heroes: Heroe[] = [
+    {
+      nombre: "Superman",
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: "Batman",
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: "Robin",
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: "Daredevil",
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: "Linterna Verde",
+      vuela: true,
+      color: Color.verde
+    }
+  ];
 
+  cambiarMayusculas() {
+    this.enMayusculas = !this.enMayusculas;
+  }
+
+  cambiarOrden(valor: string) {
+    this.ordenarPor = valor;
+  }
 }
