1e12f7042fcdd78bae5943c7f82fad3267fe5772
hectorgtez <hectorgtez@gmail.com>
2023-01-25 16:59:32 -0700

    [ADD] Avance HeoresApp - Paginas de listado, detalles y buscador terminadas

--- 02 Angular/05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
+++ 02 Angular/05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+import { Heroe } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
-  templateUrl: './buscar.component.html'
+  templateUrl: './buscar.component.html',
+  styles: [`
+    .mt {
+      margin-top: 15px;
+    }
+  `]
 })
 export class BuscarComponent {
+  termino: string = "";
+  heroes: Heroe[] = [];
+  heroeSeleccionado: Heroe | undefined;
 
+  constructor(private heroesService: HeroesService) { }
+
+  buscando(): void {
+    this.heroesService.getSugerencias(this.termino.trim())
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent): void {
+    if(!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroesService.getHeroePorId(heroe.id!)
+      .subscribe(heroe => this.heroeSeleccionado = heroe);
+  }
 }
--- 02 Angular/05-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 02 Angular/05-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Heroe } from '../interfaces/heroe.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
+  private baseUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
-  getHeroes() {
-    return this.http.get("http://localhost:3000/heroes");
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroePorId(id: string): Observable<Heroe> {
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
+  }
+
+  getSugerencias(termino: string): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${termino}&_limit=6`);
   }
 }
