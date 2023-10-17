e8092e4b5e7460c8130d928026d4f61e9d366c48
hectorgtez <hectorgtez@gmail.com>
2023-10-16 17:04:48 -0700

    [ADD] MapsApp - Estructura del side menu, configuracion de .env y README actualizado

---
+++ 02 Angular/09-mapsApp/scripts/set-envs.js
@@ -0,0 +1,13 @@
+const { writeFileSync, mkdirSync } = require('fs');
+
+require('dotenv').config();
+
+const targetPath = './src/environments/environments.ts';
+const envFileContent = `
+export const environments = {
+  MAPBOX_KEY: '${ process.env['MAPBOX_KEY'] }',
+}
+`;
+
+mkdirSync('./src/environments', { recursive: true });
+writeFileSync( targetPath, envFileContent );
--- 02 Angular/09-mapsApp/src/app/maps/components/side-menu/side-menu.component.ts
+++ 02 Angular/09-mapsApp/src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  route: string;
+}
+
 @Component({
   selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
-
+  public menuItems: MenuItem[] = [
+    { name: 'Fullscreen', route: '/maps/fullscreen' },
+    { name: 'Zoom Range', route: '/maps/zoom-range' },
+    { name: 'Markers', route: '/maps/markers' },
+    { name: 'Houses', route: '/maps/properties' },
+  ];
 }
