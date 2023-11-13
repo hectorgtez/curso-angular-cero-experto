7313c7c271dac4885fbb50e02a6631a8147b7ad8
Hector Gutierrez <hectorgtez@gmail.com>
2023-11-13 16:50:55 -0700

    [ADD] MapsApp - Agregar y quitar marcadores

--- 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
+
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
 
 @Component({
   templateUrl: './markers-page.component.html',
@@ -6,5 +12,59 @@ import { Component } from '@angular/core';
   host: {'collision-id': 'Markers'}
 })
 export class MarkersPageComponent {
+  @ViewChild('map')
+  public divMap?: ElementRef;
 
+  public markers: MarkerAndColor[] = [];
+
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-107.654, 24.818);
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 14.5, // starting zoom
+    });
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Héctor Gutiérrez';
+
+    // const marker = new Marker({
+    //   color: 'red',
+    //   element: markerHtml
+    // })
+    //   .setLngLat( this.currentLngLat )
+    //   .addTo( this.map );
+  }
+
+  createMarker(): void {
+    if ( !this.map ) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+
+    this.addMarker( lngLat, color );
+  }
+
+  addMarker( lngLat: LngLat, color: string ): void {
+    if ( !this.map ) return;
+
+    const marker = new Marker({
+      color: color,
+      draggable: true
+    })
+      .setLngLat( lngLat )
+      .addTo( this.map );
+
+    this.markers.push({ color, marker });
+  }
+
+  deleteMarker(index: number): void {
+    this.markers[index].marker.remove;
+    this.markers.splice( index, 1 );
+  }
 }
