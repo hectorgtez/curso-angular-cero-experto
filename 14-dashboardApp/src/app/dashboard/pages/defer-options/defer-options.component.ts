002477e7b1964673489cbfb2ef11ecc2d559cece
Hector Gutierrez <hectorgtez@gmail.com>
2023-12-13 17:03:51 -0700

    [ADD] DashboardApp - ChangeDetection, DeferViews y DeferOptions

--- 14-dashboardApp/src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ 14-dashboardApp/src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { TitleComponent } from '@shared/title/title.component';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, TitleComponent, HeavyLoadersFastComponent ],
   templateUrl: './defer-options.component.html',
   styles: ``
 })
--- 14-dashboardApp/src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ 14-dashboardApp/src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { TitleComponent } from '@shared/title/title.component';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, TitleComponent, HeavyLoadersSlowComponent ],
   templateUrl: './defer-views.component.html',
   styles: ``
 })
